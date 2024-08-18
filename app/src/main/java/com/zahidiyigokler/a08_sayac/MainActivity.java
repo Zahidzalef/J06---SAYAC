c078855da2b391cca53c70f47397f8897e91bde4
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2024-08-18 14:03:03 +0300

    Files have been simplified.

--- app/src/main/java/com/zahidiyigokler/a08_sayac/MainActivity.java
+++ app/src/main/java/com/zahidiyigokler/a08_sayac/MainActivity.java
@@ -3,17 +3,11 @@ package com.zahidiyigokler.a08_sayac;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.os.CountDownTimer;
-import android.support.v4.app.*;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-
     TextView textView;
 
     @Override
@@ -21,20 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         textView = findViewById(R.id.textView);
         new CountDownTimer(10000,1000) {
-
             @Override
             public void onTick(long millisUntilFinished) {
                 textView.setText("Left : "+millisUntilFinished/1000);
             }
-
             @Override
             public void onFinish() {
                 Toast.makeText(getApplicationContext(),"Done!",Toast.LENGTH_LONG).show();
